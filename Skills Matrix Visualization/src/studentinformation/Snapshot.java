65d97c8a857cc47063c825208a245a52a9878c99
Aaron Jacobson <alj@hushmail.me>
2015-05-28 13:16:49 -0700

    Added javadocs to all everything made in this class so far.

--- Skills Matrix Visualization/src/studentinformation/Snapshot.java
+++ Skills Matrix Visualization/src/studentinformation/Snapshot.java
@@ -1,36 +1,64 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package studentinformation;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 /**
- *
- * @author ros_aljacobson001
+ * A snapshot holds a date and the skills matrix information which was
+ * gathered on that date. The skills matrix information is stored in 
+ * an ArrayList.
+ * @author Aaron Jacobson
  */
 public class Snapshot {
     private Date date;
     private ArrayList<Skill> skills;
+
+    /**
+     *
+     * @param date The date that the information for the snapshot was gathered
+     * @param skills The information about the skills that were gathered
+     * @author Aaron Jacobson
+     */
     public Snapshot(Date date,ArrayList<Skill> skills){
         this.date = date;
         this.skills = skills;
     }
     
+    /**
+     * Sets the date to the new given date
+     * @param date The new date to set, as of creating this method I don't see 
+     * any use for this method but made it just in case
+     * @author Aaron Jacobson
+     */
     public void setDate(Date date){
         this.date = date;
     }
     
+    /**
+     * Sets the skills to an entirely new ArrayList
+     * @param skills The ArrayList with all the skills
+     * @author Aaron Jacobson
+     */
     public void setSkills(ArrayList<Skill> skills){
         this.skills = skills;
     }
     
-    public Date getDate(Date date){
+    /**
+     * Returns the date for this snapshot
+     * @return The date for this snapshot
+     * @author Aaron Jacobson
+     */
+    public Date getDate(){
         return this.date;
     }
     
+    /**
+     * Returns the ArrayList of skills associated with this snapshot.
+     * If you want to modify the skills it would be best to use this method then
+     * modify the ArrayList.
+     * @return The ArrayList of skills.
+     * @author Aaron Jacobson
+     */
     public ArrayList<Skill> getSkills(){
         return this.skills;
     }
