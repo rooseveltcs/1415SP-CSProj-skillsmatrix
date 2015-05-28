8eab56eff4855474202f00fd29df69501f66807b
Aaron Jacobson <alj@hushmail.me>
2015-05-28 16:58:47 -0700

    Finished the javadocs for this class.

--- Skills Matrix Visualization/src/studentinformation/Skill.java
+++ Skills Matrix Visualization/src/studentinformation/Skill.java
@@ -12,10 +12,11 @@ public class Skill {
     private int interestLevel;
 
     /**
-     *
-     * @param name
-     * @param level
-     * @param interestLevel
+     * The constructor which initializes all the class variables
+     * @param name The name of the skill
+     * @param level The number which represents skill level
+     * @param interestLevel The number which represents interest level in the skill
+     * @author Aaron Jacobson
      */
     public Skill(String name,int level,int interestLevel){
         this.name = name;
@@ -24,48 +25,57 @@ public class Skill {
     }
     
     /**
-     *
-     * @param name
+     * Sets the name of the skill. As of the creation of this class I don't see
+     * a reason to have to change the name of a skill during the program but I
+     * made the method just incase.
+     * @param name The name which it will be changed to
+     * @author Aaron Jacobson
      */
     public void setName(String name){
         this.name = name;
     }
     
     /**
-     *
-     * @param level
+     * Sets the level of the skill. As of the creation of this class I don't see
+     * a reason to have to change the level of a skill post-initialization but I
+     * made the method just incase.
+     * @param level The level which it will be set to
+     * @author Aaron Jacobson.
      */
     public void setLevel(int level){
         this.skillLevel = level;
     }
     
     /**
-     *
-     * @param interestLevel
+     * Sets the interest level in the skill. As of creation of this class I don't
+     * see a reason to have to change the interest level in a skill post-initialization
+     * but I made the method just incase.
+     * @param interestLevel The level of interest it will be set to
+     * @author Aaron Jacobson
      */
     public void setInterestLevel(int interestLevel){
         this.interestLevel = interestLevel;
     }
     
     /**
-     *
-     * @return
+     * @return the name of the skill
+     * @author Aaron Jacobson
      */
     public String getName(){
         return this.name;
     }
     
     /**
-     *
-     * @return
+     * @return the level of skill
+     * @author Aaron Jacobson
      */
     public int getLevel(){
         return this.skillLevel;
     }
     
     /**
-     *
-     * @return
+     * @return the interest in the skill
+     * @author Aaron Jacobson
      */
     public int getInterestLevel(){
         return this.interestLevel;
