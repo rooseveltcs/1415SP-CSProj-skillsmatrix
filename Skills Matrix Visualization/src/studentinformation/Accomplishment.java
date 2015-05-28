50fc3fe326d86d8aa31349cdc2f7442614573b79
Aaron Jacobson <alj@hushmail.me>
2015-05-28 13:19:59 -0700

    Added the format of the javadocs, still need to finish the rest.

--- Skills Matrix Visualization/src/studentinformation/Accomplishment.java
+++ Skills Matrix Visualization/src/studentinformation/Accomplishment.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package studentinformation;
 
 import java.util.Date;
@@ -14,23 +10,44 @@ public class Accomplishment {
     private String description;
     private Date date;
     
+    /**
+     *
+     * @param description
+     * @param date
+     */
     public Accomplishment(String description,Date date){
         this.description = description;
         this.date = date;
     }
     
+    /**
+     *
+     * @param description
+     */
     public void setDescription(String description){
         this.description = description;
     }
     
+    /**
+     *
+     * @param date
+     */
     public void setDate(Date date){
         this.date = date;
     }
     
+    /**
+     *
+     * @return
+     */
     public String getDescription(){
         return this.description;
     }
     
+    /**
+     *
+     * @return
+     */
     public Date getDate(){
         return this.date;
     }
