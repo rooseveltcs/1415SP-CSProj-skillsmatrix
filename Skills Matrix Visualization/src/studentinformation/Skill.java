610fa9b4ce67d67051a19f087188ee0c26f953ed
Aaron Jacobson <alj@hushmail.me>
2015-05-28 13:19:41 -0700

    Added some javadocs, still need to finish the rest

--- Skills Matrix Visualization/src/studentinformation/Skill.java
+++ Skills Matrix Visualization/src/studentinformation/Skill.java
@@ -1,43 +1,72 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package studentinformation;
 
 /**
- *
- * @author ros_aljacobson001
+ * This class holds the name of the skill associated with it, and the skill 
+ * level and interest level of the person at the time of gathering the 
+ * information.
+ * @author Aaron Jacobson
  */
 public class Skill {
     private String name;
     private int skillLevel;
     private int interestLevel;
+
+    /**
+     *
+     * @param name
+     * @param level
+     * @param interestLevel
+     */
     public Skill(String name,int level,int interestLevel){
         this.name = name;
         this.skillLevel = level;
         this.interestLevel = interestLevel;
     }
     
+    /**
+     *
+     * @param name
+     */
     public void setName(String name){
         this.name = name;
     }
     
+    /**
+     *
+     * @param level
+     */
     public void setLevel(int level){
         this.skillLevel = level;
     }
     
+    /**
+     *
+     * @param interestLevel
+     */
     public void setInterestLevel(int interestLevel){
         this.interestLevel = interestLevel;
     }
     
+    /**
+     *
+     * @return
+     */
     public String getName(){
         return this.name;
     }
     
+    /**
+     *
+     * @return
+     */
     public int getLevel(){
         return this.skillLevel;
     }
     
+    /**
+     *
+     * @return
+     */
     public int getInterestLevel(){
         return this.interestLevel;
     }
