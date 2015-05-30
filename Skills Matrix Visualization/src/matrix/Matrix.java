2281cebe6c4cfe564448ba51e516442afb5f5cf8
Aaron Jacobson <alj@hushmail.me>
2015-05-29 17:13:56 -0700

    Added the organizeStudentsBySkillLevel and assembleDates methods.

--- Skills Matrix Visualization/src/matrix/Matrix.java
+++ Skills Matrix Visualization/src/matrix/Matrix.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package matrix;
 
 import java.io.File;
@@ -9,8 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import studentinformation.Accomplishment;
 import studentinformation.Snapshot;
 import studentinformation.Student;
 
@@ -19,38 +14,77 @@ import studentinformation.Student;
  * @author Aaron Jacobson
  */
 public class Matrix {
+
     private File matrixFile;
     private ArrayList<Student> students;
     private ArrayList<Date> dates;
-    
-    public Matrix(File matrixFile){
+    private Date currentDate;
+
+    /**
+     * The constructor which initializes the class fields
+     * @param matrixFile The File that the matrix is in, as of writing this method
+     * there is no file reading
+     * @author Aaron Jacobson
+     */
+    public Matrix(File matrixFile) {
         this.matrixFile = matrixFile;
-    }
-    
-    public void readFromFie(){
         this.students = new ArrayList<>();
+        this.dates = new ArrayList<>();
+    }
+
+    /**
+     * WIP will read the file and use the information to construct the matrix
+     * @author Aaron Jacobson
+     */
+    public void readFromFie() {
         try {
             Scanner fileScanner = new Scanner(this.matrixFile);
         } catch (FileNotFoundException ex) {
             System.out.println("Matrix: You gave me a file which doesn't exist!");
         }
     }
-    
-    public void organizeStudentsBySkillLevel(String skillName){
-        for(int i = 0;i<students.size();i++){
-            for(int k =i+1;k<students.size();k++){
-//                if(students.get(k).g < students.get(i)){
-                
-//                }
+
+    /**
+     * Will organize the Students by skill level in the given skill least to greatest
+     * @param skillName The name of the skill to compare the Students by
+     * @author Aaron Jacobson
+     */
+    public void organizeStudentsBySkillLevel(String skillName) {
+        for (int i = 0; i < students.size(); i++) {
+            Student currentStudent = students.get(i);
+            for (int k = students.size() - 1; k > i; k--) {
+                Student compareStudent = students.get(k);
+                if (currentStudent.getSnapshot(currentDate).getSkill(skillName).getLevel() > compareStudent.getSnapshot(currentDate).getSkill(skillName).getLevel()) {
+                    i--;
+                    if (k >= students.size() - 1) {
+                        students.add(currentStudent);
+                    } else {
+                        students.add(k + 1, currentStudent);
+                    }
+                }
             }
         }
     }
-    
-    public void assembleDates(){
-        for(Student s: students){
-            for(Snapshot sn : s.getSnapshots()){
-                
+
+    /**
+     * Goes through all the snapshots and accomplishments from every student in
+     * the matrix and gathers all the dates that can be found. It shouldn't
+     * add the same date twice.
+     * @author Aaron Jacobson
+     */
+    public void assembleDates() {
+        for (Student s : students) {
+            for (Snapshot sn : s.getSnapshots()) {
+                if (!dates.contains(sn.getDate())) {
+                    dates.add(sn.getDate());
+                }
+            }
+            for(Accomplishment a : s.getAccomplishments()){
+                if(!dates.contains(a.getDate())){
+                    dates.add(a.getDate());
+                }
             }
         }
+        currentDate = dates.get(dates.size() - 1);
     }
 }
