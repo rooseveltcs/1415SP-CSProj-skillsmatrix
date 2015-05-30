a59c6792c4ffa33e921620be267d50f7559ca9ee
Aaron Jacobson <alj@hushmail.me>
2015-05-29 17:04:50 -0700

    Added get methods for all the class fields

--- Skills Matrix Visualization/src/studentinformation/Student.java
+++ Skills Matrix Visualization/src/studentinformation/Student.java
@@ -17,6 +17,7 @@ public class Student{
     private int buildSeasons;
     private ArrayList<Snapshot> snapshots;
     private ArrayList<Accomplishment> accomplishments;
+    private int favoritePokemonNumber;
     
     
     /**
@@ -26,11 +27,13 @@ public class Student{
      * @param isMale if true the student is male if not they are female
      * @param grade what grade the student is in
      * @param buildSeasons the number of build seasons that the student has participated in
+     * @param snapshots The snapshots of the Student's skills
+     * @param accomplishments A list of accomplishments the Student has completed
+     * @param favoritePokemonNumber The pokedex number of the Student's favorite pokemon
      * 
-     * 
-     * @author ros_haremash
+     * @author ros_haremash, Aaron Jacobson
      */
-    public Student(String firstName, String lastName, String race, boolean isMale, int grade, int buildSeasons,ArrayList<Snapshot> snapshots,ArrayList<Accomplishment> accomplishments){
+    public Student(String firstName, String lastName, String race, boolean isMale, int grade, int buildSeasons,ArrayList<Snapshot> snapshots,ArrayList<Accomplishment> accomplishments,int favoritePokemonNumber){
         this.firstName = firstName;
         this.lastName = lastName;
         this.race = race;
@@ -39,8 +42,12 @@ public class Student{
         this.buildSeasons = buildSeasons;
         this.snapshots = snapshots;
         this.accomplishments = accomplishments;
+        this.favoritePokemonNumber = favoritePokemonNumber;
     }
     
+    /**
+     * @return The snapshot with the most recent date associated with it
+     */
     public Snapshot getMostRecentSnapshot(){
         Snapshot recent = snapshots.get(0);
         for(Snapshot s : snapshots){
@@ -51,6 +58,12 @@ public class Student{
         return recent;
     }
     
+    /**
+     * @param date The date of the snapshot to find
+     * @return Will return the first snapshot with the given date, if there is no
+     * snapshot with the given date it will return null
+     * @author Aaron Jacobson
+     */
     public Snapshot getSnapshot(Date date){
         for(Snapshot s : snapshots){
             if(s.getDate().equals(date)){
@@ -60,35 +73,78 @@ public class Student{
         return null;
     }
     
+    /**
+     * @return The Student's first name
+     * @author Aaron Jacobson
+     */
     public String getFirstName(){
         return this.firstName;
     }
     
+    /** 
+     * @return The Student's last name
+     * @author Aaron Jacobson
+     */
     public String getLastName(){
         return this.lastName;
     }
     
+    /**
+     * @return The Student's race
+     * @author Aaron Jacobson
+     */
     public String getRace(){
         return this.race;
     }
     
+    /**
+     * @return Will return true if the Student is male, false if not
+     * @author Aaron Jacobson
+     */
     public boolean getIsMale(){
         return this.isMale;
     }
     
+    /**
+     * @return The grade the student is currently in
+     * @author Aaron Jacobson
+     */
     public int getGrade(){
         return this.grade;
     }
     
+    /**
+     * @return The number of build season's the student has participated in
+     * @author Aaron Jacobson
+     */
     public int getBuildSeasons(){
         return this.buildSeasons;
     }
     
+    /**
+     * @return The ArrayList which holds all of the snapshots associated with 
+     * the Student
+     * @author Aaron Jacobson
+     */
     public ArrayList<Snapshot> getSnapshots(){
         return this.snapshots;
     }
     
+    /**
+     * @return The ArrayList which holds all the accomplishments associated with
+     * the Student
+     * @author Aaron Jacobson
+     */
     public ArrayList<Accomplishment> getAccomplishments(){
         return this.accomplishments;
+    }
+    
+    /**
+     * @return The pokedex number of the Student's favorite pokemon, as of writing
+     * this method I only have the first 649 sprites downloaded
+     * @author Aaron Jacobson
+     */
+    public int getFavoritePokemonNumber(){
+        return this.favoritePokemonNumber;
     }
 }
